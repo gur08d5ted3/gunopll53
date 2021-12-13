caf7175c24a681a4912225fff9a246bcae3ba2b7
gur08d5ted3 <96063509+gur08d5ted3@users.noreply.github.com>
2021-12-13 14:56:01 +0000

    Add files via upload

---
+++ usermode/324. Wiggle Sort II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        if(nums.empty()) return;
+        const size_t n = nums.size();
+        vector<int> temp(n);
+        sort(nums.begin(), nums.end());
+        int i = (n + 1) >> 1, j = n;
+        for(int k = 0; k < n; ++k)
+           temp[k] = (k & 1 == 0) ? nums[--i] : nums[--j];
+        nums.swap(temp);
+    }
+};
